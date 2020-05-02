8c408c0507276d6b126ba9765ab5e4e825d9fe15
Minhajuddink7 <minhajuddink7@gmail.com>
2020-05-02 14:21:01 +0530

    Updates

---
+++ precache-manifest.3f895a8bf9954e0d86cd2f133926fa68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da431fdc348a8bc18a0a29b739b85010",
+    "url": "/Minhajuddink7/image-gallery/index.html"
+  },
+  {
+    "revision": "421e85bf3596cef5e957",
+    "url": "/Minhajuddink7/image-gallery/static/css/main.832c525a.chunk.css"
+  },
+  {
+    "revision": "94585dcacab6a5706be6",
+    "url": "/Minhajuddink7/image-gallery/static/js/2.52264eca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Minhajuddink7/image-gallery/static/js/2.52264eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "421e85bf3596cef5e957",
+    "url": "/Minhajuddink7/image-gallery/static/js/main.246ce329.chunk.js"
+  },
+  {
+    "revision": "fbef964d325b9070742d",
+    "url": "/Minhajuddink7/image-gallery/static/js/runtime-main.bf09eece.js"
+  }
+]);
